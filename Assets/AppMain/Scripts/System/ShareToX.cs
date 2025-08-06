c009f58cf0429dd8a8b991888584411e752d9bb1
bellbelljp <n.skm13@gmail.com>
2025-08-06 14:24:58 +0900

    変更：シェアの処理を更新

--- Assets/AppMain/Scripts/System/ShareToX.cs
+++ Assets/AppMain/Scripts/System/ShareToX.cs
@@ -1,13 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Runtime.InteropServices;
 using System.Web; // .NET Framework 用のエンコード（必要に応じて）
+using UnityEngine;
 
 namespace Tarot
 {
 	public class ShareToX : MonoBehaviour
 	{
+		// JavaScript関数のインポート（WebGLでのみ有効）
+		[DllImport("__Internal")]
+		private static extern void OpenTwitterWindow(string url);
+
 		// シェアしたいテキストとURL（改行などもOK）
-		string tweetText = "タロットをプレイしたよ！#Unity1Week";
-		string tweetURL = "https://unityroom.com/games/bellbelljp"; // 任意（ストアページなど）
+		string tweetText = "タロットをプレイしたよ！#unity1week";
+		string tweetURL = "https://unityroom.com/games/tarot";
+		string[] tweetTags = { "unity1room" };
+		string gameId = "tarot";
 
 		public void Share()
 		{
@@ -17,7 +24,11 @@ namespace Tarot
 
 			string twitterURL = $"https://twitter.com/intent/tweet?text={text}&url={url}";
 
+#if UNITY_WEBGL && !UNITY_EDITOR
+			        OpenTwitterWindow(twitterURL);
+#else
 			Application.OpenURL(twitterURL);
+#endif
 		}
 	}
 }
